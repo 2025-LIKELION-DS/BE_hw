febfecf743467ab862b1295717311d5669001131
yslee <yeongseo6142@naver.com>
2025-03-28 18:21:58 +0900

    [0328] 이영서 - 피드백&네임스페이스 적용

--- wordcount/wordcount_prj/wordcount_prj/urls.py
+++ wordcount/wordcount_prj/wordcount_prj/urls.py
@@ -16,13 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-from wordcount import views
+from django.urls import path, include
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', views.index, name='index'),
-    path("word-count/", views.word_count, name='word_count'),
-    path("result/", views.result, name="result"),
-    path("hello/", views.hello, name="hello"),
+    path('', include('wordcount.urls')),
 ]
