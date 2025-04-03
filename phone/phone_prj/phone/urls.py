b059599962fa4859276153de6c8258ed5182f1e0
yslee <yeongseo6142@naver.com>
2025-04-03 18:19:56 +0900

    [0403] 이영서 - 스터디 시험 (CRUD 추가)

--- phone/phone_prj/phone/urls.py
+++ phone/phone_prj/phone/urls.py
@@ -6,5 +6,9 @@ app_name="phone"
 urlpatterns=[
     path('', list, name='list'),
     path('result/', result, name='result'),
+    path('create/', create, name='create'),
+    path('update/<int:id>/', update, name='update'),
+    path('detail/<int:id>/', detail, name='detail'),
+    path('delete/<int:id>/', delete, name='delete'),
 ]
 
