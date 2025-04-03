801d9de706e0c83beb05826201c96f28c647bfd2
rhdbqls <kyb193539@naver.com>
2025-04-03 14:50:04 +0900

    [0403]고유빈-phone 피드백 수정

--- phone/phone_prj/phone/views.py
+++ phone/phone_prj/phone/views.py
@@ -9,13 +9,7 @@ def list(request):
     # 'list.html' x -> templates>phone>list.html이므로 phone/list.html
 
 def result(request):
-    if request.method=='GET':
-        keyword=request.GET.get('keyword') # 검색어 부분 받기
-        result=Phone.objects.all() # 모든 전화번호 list 받기
-
-        if keyword:
-            result=result.filter(name__contains=keyword).order_by('name')
-            # 이름 필터링 
-             # //필드__contains : 대소문자 구분, 특정 문자열 포함되어 있는지 확인
+    keyword=request.GET.get('keyword') # 검색어 부분 받기
+    result = Phone.objects.filter(name__contains = keyword).order_by('name')
             
     return render(request, 'phone/result.html',{'result':result,'keyword':keyword})
